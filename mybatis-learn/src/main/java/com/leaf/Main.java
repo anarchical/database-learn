9e9ad2e9b4f65c09b53f1a27de9535e482da6189
leaf <920201895@qq.com>
2022-09-18 00:54:49 +0800

    init

--- mybatis-learn/src/main/java/com/leaf/Main.java
+++ mybatis-learn/src/main/java/com/leaf/Main.java
@@ -1,6 +1,9 @@
-package com.leaf;/**
+package com.leaf;
+
+/**
  * @author yyq
- */public class Main {
+ */
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
